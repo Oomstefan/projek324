1a4b804fc6284567e32bcad78fb29e0a4a05b989
Oomstefan <30688751+Oomstefan@users.noreply.github.com>
2017-08-08 15:24:00 +0200

    Add files via upload
    project upload

---
+++ projek324/324projek/324projek/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace _324projek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStefan = new System.Windows.Forms.Button();
+            this.btnCharl = new System.Windows.Forms.Button();
+            this.btnRuan = new System.Windows.Forms.Button();
+            this.btnRuanS = new System.Windows.Forms.Button();
+            this.btnJean = new System.Windows.Forms.Button();
+            this.btnTiaan = new System.Windows.Forms.Button();
+            this.btnHenko = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStefan
+            // 
+            this.btnStefan.Location = new System.Drawing.Point(44, 35);
+            this.btnStefan.Name = "btnStefan";
+            this.btnStefan.Size = new System.Drawing.Size(75, 23);
+            this.btnStefan.TabIndex = 0;
+            this.btnStefan.Text = "Stefan";
+            this.btnStefan.UseVisualStyleBackColor = true;
+            // 
+            // btnCharl
+            // 
+            this.btnCharl.Location = new System.Drawing.Point(158, 35);
+            this.btnCharl.Name = "btnCharl";
+            this.btnCharl.Size = new System.Drawing.Size(75, 23);
+            this.btnCharl.TabIndex = 1;
+            this.btnCharl.Text = "Charl";
+            this.btnCharl.UseVisualStyleBackColor = true;
+            // 
+            // btnRuan
+            // 
+            this.btnRuan.Location = new System.Drawing.Point(273, 35);
+            this.btnRuan.Name = "btnRuan";
+            this.btnRuan.Size = new System.Drawing.Size(75, 23);
+            this.btnRuan.TabIndex = 2;
+            this.btnRuan.Text = "Ruan v Zyl";
+            this.btnRuan.UseVisualStyleBackColor = true;
+            // 
+            // btnRuanS
+            // 
+            this.btnRuanS.Location = new System.Drawing.Point(44, 101);
+            this.btnRuanS.Name = "btnRuanS";
+            this.btnRuanS.Size = new System.Drawing.Size(75, 23);
+            this.btnRuanS.TabIndex = 3;
+            this.btnRuanS.Text = "Ruan S";
+            this.btnRuanS.UseVisualStyleBackColor = true;
+            // 
+            // btnJean
+            // 
+            this.btnJean.Location = new System.Drawing.Point(158, 101);
+            this.btnJean.Name = "btnJean";
+            this.btnJean.Size = new System.Drawing.Size(75, 23);
+            this.btnJean.TabIndex = 4;
+            this.btnJean.Text = "Jean";
+            this.btnJean.UseVisualStyleBackColor = true;
+            // 
+            // btnTiaan
+            // 
+            this.btnTiaan.Location = new System.Drawing.Point(273, 101);
+            this.btnTiaan.Name = "btnTiaan";
+            this.btnTiaan.Size = new System.Drawing.Size(75, 23);
+            this.btnTiaan.TabIndex = 5;
+            this.btnTiaan.Text = "Tiaan";
+            this.btnTiaan.UseVisualStyleBackColor = true;
+            // 
+            // btnHenko
+            // 
+            this.btnHenko.Location = new System.Drawing.Point(158, 174);
+            this.btnHenko.Name = "btnHenko";
+            this.btnHenko.Size = new System.Drawing.Size(75, 23);
+            this.btnHenko.TabIndex = 6;
+            this.btnHenko.Text = "Henko";
+            this.btnHenko.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 343);
+            this.Controls.Add(this.btnHenko);
+            this.Controls.Add(this.btnTiaan);
+            this.Controls.Add(this.btnJean);
+            this.Controls.Add(this.btnRuanS);
+            this.Controls.Add(this.btnRuan);
+            this.Controls.Add(this.btnCharl);
+            this.Controls.Add(this.btnStefan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStefan;
+        private System.Windows.Forms.Button btnCharl;
+        private System.Windows.Forms.Button btnRuan;
+        private System.Windows.Forms.Button btnRuanS;
+        private System.Windows.Forms.Button btnJean;
+        private System.Windows.Forms.Button btnTiaan;
+        private System.Windows.Forms.Button btnHenko;
+    }
+}
+
